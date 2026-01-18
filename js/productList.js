e2d7c453d73d5d6efe97b110a3cd9a7968397a05
unknown <priyanshu9555@gmail.com>
2026-01-18 23:36:47 +0530

    services details page added

--- js/productList.js
+++ js/productList.js
@@ -2,65 +2,401 @@
    PRODUCT DATA
 ================================ */
 const productData = [
-    {
+  {
         category: "Pusher Seals",
-        image: "./images/products/section1/Pusher Seals/Pusher Seals 1.png",
-        slug: "pusher-seals"
+        slug: "pusher-seals",
+        images: [
+            "./images/products/section1/Pusher Seals/Pusher Seals 1.png",
+            "./images/products/section1/Pusher Seals/Pusher Seals 2.png"
+        ],
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Single Spring Seal",
-        image: "./images/products/section1/Single Spring Seal/Single Spring Seal 1.jpg",
-        slug: "single-spring"
+        images: [
+            "./images/products/section1/Single Spring Seal/Single Spring Seal 1.jpg",
+            "./images/products/section1/Single Spring Seal/Single Spring Seal 2.jpg",
+        ],
+        slug: "single-spring",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Multi Spring Seal",
-        image: "./images/products/section1/Multi Spring Seal/Multi-Spring-Seal-1.jpg",
-        slug: "multi-spring"
+        images: [
+            "./images/products/section1/Multi Spring Seal/Multi-Spring-Seal-1.jpg",
+            "./images/products/section1/Multi Spring Seal/Multi-Spring-Seal-2.jpg",
+        ],
+        slug: "multi-spring",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Textile Seal",
-        image: "./images/products/section1/Textile Seal/Textile-Seal---1.png",
-        slug: "textile-seal"
+        images: [
+            "./images/products/section1/Textile Seal/Textile-Seal1.png",
+            "./images/products/section1/Textile Seal/Textile-Seal---1.png",
+        ],
+        slug: "textile-seal",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Teflon Bellow Seals",
-        image: "./images/products/section1/Teflon Bellow Seals/Teflon-Bellow-Seals--1.png",
-        slug: "teflon-bellow"
+        images: [
+            "./images/products/section1/Teflon Bellow Seals/Teflon-Bellow-Seals--1.png",
+            "./images/products/section1/Teflon Bellow Seals/Teflon-Bellow-Seals---2.png",
+        ],
+        slug: "teflon-bellow",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Metal Bellow Mechanical",
-        image: "./images/products/section1/Metal Bellow Mechanical/Metal-Bellow-Mechanical---1.png",
-        slug: "metal-bellow"
+        images: [
+            "./images/products/section1/Metal Bellow Mechanical/Metal-Bellow-Mechanical---1.png",
+            "./images/products/section1/Metal Bellow Mechanical/Metal-Bellow-Mechanical---2.png",
+        ],
+        slug: "metal-bellow",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Boiler Feed Water Seals",
-        image: "./images/products/section1/Boiler Feed Water Seals/Boiler-Feed-Water-Seals---1.png",
-        slug: "boiler-feed"
+        images: [
+            "./images/products/section1/Boiler Feed Water Seals/Boiler-Feed-Water-Seals---1.png",
+            "./images/products/section1/Boiler Feed Water Seals/Boiler-Feed-Water-Seals---2.png",
+        ],
+        slug: "boiler-feed",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Split Seals",
-        image: "./images/products/section1/Split Seals/Split-Seals---1.png",
-        slug: "split-seals"
+        images: [
+            "./images/products/section1/Split Seals/Split-Seals---1.png",
+            "./images/products/section1/Split Seals/Split-Seals---2.png",
+        ],
+        slug: "split-seals",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "High Pressure Seal",
-        image: "./images/products/section1/High Pressure Seal/High-Pressure-Seal--1.png",
-        slug: "high-pressure"
+        images: [
+            "./images/products/section1/High Pressure Seal/High-Pressure-Seal--1.png",
+            "./images/products/section1/High Pressure Seal/High-Pressure-Seal---2.png",
+        ],
+        slug: "high-pressure",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Dry Seals",
-        image: "./images/products/section1/Dry Seals/Dry-Seals--1.png",
-        slug: "dry-seals"
+        images: [
+            "./images/products/section1/Dry Seals/Dry-Seals--1.png",
+            "./images/products/section1/Dry Seals/Dry-Seals---2.png",
+        ],
+        slug: "dry-seals",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Cartridge Single Acting",
-        image: "./images/products/section1/Cartridge Single Acting/Cartridge-Single-Acting--1.png",
-        slug: "cartridge-single"
+        images: [
+            "./images/products/section1/Cartridge Single Acting/Cartridge-Single-Acting--1.png",
+            "./images/products/section1/Cartridge Single Acting/Cartridge-Single-Acting--2.png",
+        ],
+        slug: "cartridge-single",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     },
     {
         category: "Cartridge Double Acting",
-        image: "./images/products/section1/Cartridge Double Acting/Cartridge-Double-Acting---1.png",
-        slug: "cartridge-double"
+        images: [
+            "./images/products/section1/Cartridge Double Acting/Cartridge-Double-Acting---1.png",
+            "./images/products/section1/Cartridge Double Acting/Cartridge-Double-Acting---2.png",
+        ],
+        slug: "cartridge-double",
+        details: {
+            standardStyle: [
+                "Face Materials: Stellite, Carbon, Ceramic, Tungsten Carbide, Silicon Carbide, Lecrolly",
+                "Metal Parts: SS 316, SS 304",
+                "Secondary Seal: Elastomers, PTFE"
+            ],
+            characteristics: [
+                "Single acting",
+                "Unbalanced",
+                "Inside mounted",
+                "Dependent on direction of rotation"
+            ],
+            operatingLimits: [
+                "Shaft Diameter D1: 10 mm to 100 mm",
+                "Pressure P: 0.8 bar (max)",
+                "Temperature t: -20°C to +200°C",
+                "Velocity V: 20 m/sec"
+            ],
+            applications: [
+                "Water Pumps",
+                "Circulation Pumps for central heating",
+                "Chemical Process Pumps",
+                "Sewage and Submersible pumps"
+            ]
+        }
     }
 ];
 
@@ -100,6 +436,81 @@ function renderCategories() {
     });
 }
 
+function showProductDetails(slug) {
+    const product = productData.find(p => p.slug === slug);
+    if (!product) return;
+
+    productsGrid.classList.add("details-mode");
+
+    productsGrid.innerHTML = `
+        <div class="product-details">
+
+            <button class="back-btn" onclick="backToProducts()">
+                ← Back to Products
+            </button>
+
+            <h2 class="product-title">${product.category}</h2>
+
+            <div class="product-top">
+                
+                <img src="${product.images[0]}" alt="">
+            </div>
+
+          <div class="details-section">
+    <h3>⚙ Standard Style</h3>
+    <ul>
+        ${product.details.standardStyle.map(item => {
+            const [key, value] = item.split(":");
+            return `
+                <li>
+                    <strong>${key}:</strong>
+                    <span>${value}</span>
+                </li>
+            `;
+        }).join("")}
+    </ul>
+</div>
+
+
+            <div class="details-split">
+                <div>
+                    <h3>⚙ Seal Characteristics</h3>
+                    <ul>
+                        ${product.details.characteristics.map(i => `<li>${i}</li>`).join("")}
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>⚙ Operating Limits</h3>
+                    <ul>
+                        ${product.details.operatingLimits.map(i => `<li>${i}</li>`).join("")}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="details-bottom">
+                <img src="${product.images[1]}" alt="">
+
+                <div>
+                    <h3>⚙ Applications</h3>
+                    <ul>
+                        ${product.details.applications.map(i => `<li>${i}</li>`).join("")}
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    `;
+}
+
+
+function backToProducts() {
+    productsGrid.classList.remove("details-mode");
+    renderProducts(productData);
+}
+
+
+
 /* ===============================
    RENDER PRODUCTS
 ================================ */
@@ -107,14 +518,16 @@ function renderProducts(products) {
     productsGrid.innerHTML = "";
 
     products.forEach(p => {
+        const thumbnail = p.images?.[0] || "";
+
         productsGrid.innerHTML += `
             <div class="product-card">
-                <img src="${p.image}" alt="${p.category}">
+                <img src="${thumbnail}" alt="${p.category}">
                 <h4>${p.category}</h4>
                 <div class="product-actions">
-                    <a href="product-details.html?product=${p.slug}" class="read-more">
+                    <button class="read-more" onclick="showProductDetails('${p.slug}')">
                         READ MORE →
-                    </a>
+                    </button>
                     <a href="contact.html" class="enquiry-btn-small">
                         ENQUIRY →
                     </a>
@@ -123,6 +536,8 @@ function renderProducts(products) {
         `;
     });
 }
+
+
 
 /* ===============================
    FILTER PRODUCTS
